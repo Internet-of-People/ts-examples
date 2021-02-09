3c3e39d122559196e050e040001a368f78f832b3
Rache Bartmoss <bartmoss@tutanota.com>
2021-02-09 10:23:41 +0100

    Fixed vault creation

--- morpheus/src/samples/vault-init.ts
+++ morpheus/src/samples/vault-init.ts
@@ -5,6 +5,7 @@ const unlockPassword = 'correct horse battery staple';
 
 export const vaultInit = async(path: string): Promise<void> => {
   const vault = Crypto.Vault.create(Crypto.Seed.demoPhrase(), '', unlockPassword);
+  Crypto.MorpheusPlugin.rewind(vault, unlockPassword);
   const serialized = JSON.stringify(vault.save());
   await fs.writeFile(path, serialized, { encoding: 'utf-8' });
 };
